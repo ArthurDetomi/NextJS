a37d66774bc69a3edf3900ae942d31c4ab3d6b11
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-17 20:52:11 -0300

    JSX 1 - 3

---
+++ exercicios/src/pages/basico/jsx2.js
@@ -0,0 +1,8 @@
+export default function jsx2() {
+    const conteudo = (
+        <div>
+            <h1>Jsx #02 </h1>
+        </div>
+    );
+    return conteudo;
+}
---
+++ exercicios/src/pages/basico/jsx3.jsx
@@ -0,0 +1,6 @@
+export default function jsx3() {
+    return (
+        <div>
+        </div>
+    );
+}
