bbee922a7083ed72018418fc5abb465cbe54883f
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-17 21:24:06 -0300

    Lista

--- exercicios/src/pages/basico/lista3.jsx
+++ exercicios/src/pages/basico/lista3.jsx
@@ -2,20 +2,27 @@ function gerarLista(tam) {
     let lista = [];
 
     for (let i = 1; i <= tam; i++) {
-        lista.push(<span>{i},</span>);
+        lista.push(<span key={i}>{i},</span>);
     }
 
     return lista;
 }
 
 export default function lista1() {
- 
+
 
     return (
-        <div>
-            {
-               gerarLista(10)
-            }
-        </div>
+        <>
+            <div>
+                {
+                    gerarLista(10)
+                }
+            </div>
+            <div>
+                {
+                    gerarLista(3)
+                }
+            </div>
+        </>
     );
 }
