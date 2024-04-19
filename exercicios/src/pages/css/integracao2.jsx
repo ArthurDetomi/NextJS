9a06bf39f250b87cf4902a7573ac325eaaad1cf2
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-18 21:17:29 -0300

    Aplicando css com module.css

---
+++ exercicios/src/pages/css/integracao2.jsx
@@ -0,0 +1,11 @@
+import styles from './integracao2.module.css'
+
+export default function integracao2() {
+    return (
+        <div>
+            <div className={styles.roxo}>Texto 1</div>
+            <div className={styles.laranja}>Texto 2</div>
+            <div className={styles.marrom}>Texto 3</div>
+        </div>
+    );
+}
