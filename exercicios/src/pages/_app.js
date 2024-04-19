b166915f16de742c1b3b5f1844fc65be8f971ed7
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-18 21:09:18 -0300

    Aplicando Css nos componentes

--- exercicios/src/pages/_app.js
+++ exercicios/src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "@/pages/css/integracao1.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ exercicios/src/pages/css/integracao1.jsx
@@ -0,0 +1,10 @@
+
+export default function integracao() {
+    return (
+        <div className="integracao1">
+            <div className="vermelho">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branco">Texto #03</div>
+        </div>
+    )
+}
