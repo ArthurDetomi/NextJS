e421125b7fb8240010eb97bf0dc986c7058f4abf
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-18 21:22:43 -0300

    Css específico para um componente

--- exercicios/src/pages/css/integracao2.jsx
+++ exercicios/src/pages/css/integracao2.jsx
@@ -2,7 +2,7 @@ import styles from './integracao2.module.css'
 
 export default function integracao2() {
     return (
-        <div>
+        <div id={styles.integracao2}>
             <div className={styles.roxo}>Texto 1</div>
             <div className={styles.laranja}>Texto 2</div>
             <div className={styles.marrom}>Texto 3</div>
