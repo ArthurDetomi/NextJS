7be7d933ae18fcd03819c586ef95251f9e1f558a
Arthur Detomi <arthurdetomi@gmail.com>
2024-04-18 21:32:52 -0300

    Props são somente leitura

---
+++ exercicios/src/pages/basico/propsSomenteLeitura.jsx
@@ -0,0 +1,9 @@
+import SomaUm from "../../../components/SomaUm";
+
+export default function propsSomenteLeitura() {
+    return (
+        <div>
+            <SomaUm numero={9} />
+        </div>
+    )
+}
